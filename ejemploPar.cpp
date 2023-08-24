9203353bc66b189a7ba7df65438b999ffdb06622
Javier Valle <63107636+Javier19-cmd@users.noreply.github.com>
2023-08-24 10:03:09 -0600

    agregando comentarios

--- ejemploPar.cpp
+++ ejemploPar.cpp
@@ -1,3 +1,15 @@
+/**
+ * @file ejemploPar.cpp
+ * @author Javier Valle, Mario de Leon
+ * @ID 20159, 19019.
+ * @brief 
+ * @version 0.1
+ * @date 2023-08-24
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
@@ -6,10 +18,14 @@
 #include <vector>
 #include <cmath>
 
+// Propiedades de la pantalla.
 const int SCREEN_WIDTH = 700;
 const int SCREEN_HEIGHT = 700;
+
+// Cuadros por segundo.
 const int MIN_FPS = 60;
 
+// Clase elemento.
 class Element {
 public:
     Element(int x, int y, int radius, int speedX, int speedY)
@@ -23,10 +39,12 @@ public:
         x += speedX;
         y += speedY;
 
+        // Cambio de direccion de X cuando el elemento choca con alguna "pared" de la pantalla.
         if (x - radius <= 0 || x + radius >= SCREEN_WIDTH) {
             speedX = -speedX;
         }
-
+        
+        // Cambio de direccion de Y cuando el elemento choca con alguna "pared" de la pantalla.
         if (y - radius <= 0 || y + radius >= SCREEN_HEIGHT) {
             speedY = -speedY;
         }
@@ -35,9 +53,11 @@ public:
         speedY = std::min(std::max(speedY, -5), 5);
     }
 
+    // Renderizador del elemento.
     void render(SDL_Renderer* renderer) {
         SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, 255);
         
+        // Dibujando el circulo.
         for (int i = x - radius; i <= x + radius; ++i) {
             for (int j = y - radius; j <= y + radius; ++j) {
                 int dx = i - x;
@@ -54,12 +74,14 @@ private:
     SDL_Color color;
 };
 
+// Funcion para actualizar la pos de los elementos en paralelo.
 void updateElements(Element** elements, int numElements) {
     for (int i = 0; i < numElements; ++i) {
         elements[i]->update();
     }
 }
 
+// Funcion para renderizar los elementos en paralelo.
 void renderElements(SDL_Renderer* renderer, Element** elements, int numElements) {
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
     SDL_RenderClear(renderer);
@@ -77,23 +99,28 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
+    // Creando la ventana.
     SDL_Window* window = SDL_CreateWindow("Screensaver", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
     if (!window) {
         std::cerr << "Window creation failed: " << SDL_GetError() << std::endl;
         return 1;
     }
 
+    // Creando el renderizador.
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
     if (!renderer) {
         std::cerr << "Renderer creation failed: " << SDL_GetError() << std::endl;
         return 1;
     }
 
+    // Obteniendo la cantidad de elementos que ingreso el user, si no, se usan 50 default.
     int numElements = (argc > 1) ? atoi(argv[1]) : 50;
     srand(time(nullptr));
 
+    // Creando un vector de punteros a elementos del programa.
     std::vector<Element*> elements;
     for (int i = 0; i < numElements; ++i) {
+        // Generando propiedades aleatorias por cada uno de los elementos creados.
         int x = rand() % (SCREEN_WIDTH - 30) + 30;
         int y = rand() % (SCREEN_HEIGHT - 30) + 30;
         int radius = rand() % 10 + 5;
@@ -107,21 +134,26 @@ int main(int argc, char* argv[]) {
     Uint32 startTime, frameTime, prevTime = 0;
     int frames = 0;
 
+    // Bucle principal del programa.
     while (!quit) {
         startTime = SDL_GetTicks();
 
+        // Manejando los elementos de SDL.
         while (SDL_PollEvent(&e)) {
             if (e.type == SDL_QUIT) {
                 quit = true;
             }
         }
 
+        // Creando hilos para poder actualizar y renderizar los elementos que se crearon en paralelo.
         std::thread updateThread(updateElements, elements.data(), numElements);
         std::thread renderThread(renderElements, renderer, elements.data(), numElements);
 
+        // Esperando a que los hilos terminen.
         updateThread.join();
         renderThread.join();
 
+        // Calculando e imprimiendo los FPS del programa.
         frames++;
         Uint32 currentTime = SDL_GetTicks();
         if (currentTime - prevTime >= 1000) {
@@ -130,12 +162,14 @@ int main(int argc, char* argv[]) {
             prevTime = currentTime;
         }
 
+        // Limitando la velocidad de actualizacion.
         frameTime = SDL_GetTicks() - startTime;
         if (frameTime < 1000 / MIN_FPS) {
             SDL_Delay(1000 / MIN_FPS - frameTime);
         }
     }
 
+    // Liberando la memoria y cerrando el SDL.
     for (Element* element : elements) {
         delete element;
     }
--- ejemploSec.cpp
+++ ejemploSec.cpp
@@ -1,22 +1,40 @@
+/**
+ * @file ejemploSec.cpp
+ * @author Javier Valle, Mario de Leon
+ * @ID 20159, 19019.
+ * @brief 
+ * @version 0.1
+ * @date 2023-08-24
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
 #include <SDL2/SDL.h>
 #include <cmath>
 
+// Dimensiones de la ventana.
 const int SCREEN_WIDTH = 700;
 const int SCREEN_HEIGHT = 700;
+
+// Maximo de cuadros por segundo.
 const int MIN_FPS = 60;
 
+// Clase que representa los elementos.
 class Element {
 public:
     Element(int x, int y, int radius, int speedX, int speedY)
         : x(x), y(y), radius(radius), speedX(speedX), speedY(speedY) {
+        // Color aleatorio para cada circulo a generar.
         color.r = rand() % 256;
         color.g = rand() % 256;
         color.b = rand() % 256;
     }
 
+    // Actualizador de la pos del elemento.
     void update() {
         x += speedX;
         y += speedY;
@@ -29,14 +47,16 @@ public:
             speedY = -speedY; // Cambio de dirección en Y al colisionar con las paredes verticales
         }
 
-        // Limitar la velocidad en las direcciones X e Y
+        // Limitando la velocidad en las direcciones X e Y
         speedX = std::min(std::max(speedX, -5), 5);
         speedY = std::min(std::max(speedY, -5), 5);
     }
 
+    // Renderizando el elemento en la ventana.
     void render(SDL_Renderer* renderer) {
         SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, 255);
         
+        // Dibujando el circulo.
         for (int i = x - radius; i <= x + radius; ++i) {
             for (int j = y - radius; j <= y + radius; ++j) {
                 int dx = i - x;
@@ -59,23 +79,29 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
+    // Creando la ventana del screensaver.
     SDL_Window* window = SDL_CreateWindow("Screensaver", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
     if (!window) {
         std::cerr << "Window creation failed: " << SDL_GetError() << std::endl;
         return 1;
     }
 
+    // Creando el renderizador.
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
     if (!renderer) {
         std::cerr << "Renderer creation failed: " << SDL_GetError() << std::endl;
         return 1;
     }
 
+    // Obteniendo la cantidad de elementos que ingreso el usuario. Si no ingreso nada, se ponen 50.
     int numElements = (argc > 1) ? atoi(argv[1]) : 50;
     srand(time(nullptr));
 
+    // Creando un arreglos que tiene punteros a elementos.
     Element* elements[numElements];
     for (int i = 0; i < numElements; ++i) {
+
+        // Generando propiedades aleatorias para cada elemento.
         int x = rand() % (SCREEN_WIDTH - 30) + 30;
         int y = rand() % (SCREEN_HEIGHT - 30) + 30;
         int radius = rand() % 10 + 5;
@@ -98,16 +124,20 @@ int main(int argc, char* argv[]) {
             }
         }
 
+        // Limpieando el renderizador.
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
         SDL_RenderClear(renderer);
 
+        // Actualizando y renderizando los elementos.
         for (int i = 0; i < numElements; ++i) {
             elements[i]->update();
             elements[i]->render(renderer);
         }
 
+        // Sacando el elemento renderizado en la ventana.
         SDL_RenderPresent(renderer);
 
+        // Calculando e imprimiendo los FPS de la pantalla.
         frames++;
         Uint32 currentTime = SDL_GetTicks();
         if (currentTime - prevTime >= 1000) {
@@ -116,12 +146,14 @@ int main(int argc, char* argv[]) {
             prevTime = currentTime;
         }
 
+        // Limitando la velocidad de actualizacion.
         frameTime = SDL_GetTicks() - startTime;
         if (frameTime < 1000 / MIN_FPS) {
             SDL_Delay(1000 / MIN_FPS - frameTime);
         }
     }
 
+    // Liberando la memoria y cerrando el SDL.
     for (int i = 0; i < numElements; ++i) {
         delete elements[i];
     }
